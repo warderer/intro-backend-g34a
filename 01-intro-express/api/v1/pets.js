3997f91c50036ba99df6515950f842b1697349e2
César Guerra <warderer@gmail.com>
2024-10-28 21:08:59 -0600

    ✨ Add express routes - pets

---
+++ 01-intro-express/api/v1/pets.js
@@ -0,0 +1,35 @@
+// Para definir rutas en un archivo separado, usando Express, se debe configurar un router.
+
+const express = require('express')
+// Mando a llamar a la función Router de express
+const router = express.Router()
+
+const petList = {
+    "pets": [
+        {
+            "id": 1,
+            "name": "Firulais",
+            "age": 3,
+            "type": "dog"
+        },
+        {
+            "id": 2,
+            "name": "Michi",
+            "age": 2,
+            "type": "cat"
+        },
+        {
+            "id": 3,
+            "name": "Scooby Doo",
+            "age": 6,
+            "type": "dog"
+        },
+    ]
+}
+
+// Traer la lista de todas las mascotas
+router.get('/api/v1/pets', (req, res) => {
+    res.json(petList)
+})
+
+module.exports = router
--- 01-intro-express/index.js
+++ 01-intro-express/index.js
@@ -1,6 +1,8 @@
 // #1 Llamar a la biblioteca de express (importarla)
 const express = require('express')
 
+const petsRoutes = require('./api/v1/pets')
+
 // Hago una variable para el puerto, para que sea más fácil de cambiar. Si no hay un puerto definido en las variables de entorno, se usa el 3000
 const PORT = process.env.PORT || 3000
 
@@ -17,6 +19,8 @@ app.use(express.json())
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+app.use(petsRoutes) // Le digo a express que use las rutas de pets
 
 // #4 Levantar el servidor escuchando en un puerto determinado
 app.listen(PORT, () => {
