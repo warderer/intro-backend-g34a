ecb667acb1f773316210d622c17eb8a5d5c0af20
César Guerra <warderer@gmail.com>
2024-11-04 19:41:59 -0600

    ✨ Add Home model - with create home

---
+++ 02-knexBnB-api/models/Home.js
@@ -0,0 +1,26 @@
+/* MODELO */
+// El modelo trae los datos de la base de datos.
+// NO se encarga de validar datos ni resolver promesas (eso lo hace el controlador la arquitectura MVC).
+
+// #1 Importar Knex: Traer la conexión a la base de datos adecuada.
+const knex = require('../config')
+
+// #2 Crear funciones que me permitan interactuar con la base de datos.
+// CRUD: Create, Read, Update, Delete
+
+// CREATE
+const create = (bodyHome) => {
+  return knex
+    .insert(bodyHome) // ¿Qué datos voy a insertar?
+    .into('homes') // ¿En qué tabla?
+    .returning(['house_id', 'title', 'description', 'guest', 'address', 'rental_price', 'active', 'created_at']) // ¿Qué datos quiero que me regrese?
+}
+// READ
+
+// UPDATE
+
+// DELETE
+
+module.exports = {
+  create
+}
